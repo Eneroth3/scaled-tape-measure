e3a180f364c1fd851f1e05ed73839f6864c9abaf
Eneroth3 <eneroth3@gmail.com>
2019-11-15 22:56:09 +0100

    Draw measurement arrow

--- src/ene_scaled_tape_measure/tape_measure_tool.rb
+++ src/ene_scaled_tape_measure/tape_measure_tool.rb
@@ -11,6 +11,9 @@ module Eneroth
       # Identifier for tool state for picking end point.
       STATE_MEASURE = 1
 
+      # Size of arrow head.
+      ARROW_HEAD_SIZE = 8
+
       # Scale to measure with respect to.
       @@scale ||= Scale.new("1:1")
 
@@ -41,15 +44,13 @@ module Eneroth
         @start_ip.draw(view)
         @end_ip.draw(view)
 
-        # TODO: Draw line and arrows.
-
-        view.tooltip =
-          case @state
-          when STATE_START
-            @start_ip.tooltip
-          when STATE_MEASURE
-            output
-          end
+        case @state
+        when STATE_START
+          view.tooltip = @start_ip.tooltip
+        when STATE_MEASURE
+          view.tooltip = output
+          draw_arrow(@start_ip.position, @end_ip.position, view)
+        end
       end
 
       # @api
@@ -60,18 +61,30 @@ module Eneroth
 
       # @api
       # @see https://ruby.sketchup.com/Sketchup/Tool.html
-      def onCancel(_reason, _view)
-        reset
+      def getExtents
+        bb = Sketchup.active_model.bounds
+        bb.add(@start_ip.position)
+        bb.add(@end_ip.position)
+
+        bb
       end
 
       # @api
       # @see https://ruby.sketchup.com/Sketchup/Tool.html
-      def onLButtonDown(_flags, _x, _y, _view)
+      def onCancel(_reason, _view)
+        reset
+        view.invalidate
+      end
+
+      # @api
+      # @see https://ruby.sketchup.com/Sketchup/Tool.html
+      def onLButtonDown(_flags, _x, _y, view)
         case @state
         when STATE_START
           @state = STATE_MEASURE
         when STATE_MEASURE
           reset
+          view.invalidate
         end
         update_status_text
       end
@@ -119,15 +132,38 @@ module Eneroth
       def ene_tool_cycler_icon
         # TODO: Make icon.
         File.join(PLUGIN_ROOT, "images", "tape_measure.svg")
-      end
 
       private
+
+      def draw_arrow(point1, point2, view)
+        return if point1 == point2
+
+        view.set_color_from_line(point1, point2)
+        view.draw(GL_LINES, [point1, point2])
+        draw_arrow_head(point1, measure_vector.reverse, view)
+        draw_arrow_head(point2, measure_vector, view)
+      end
+
+      def draw_arrow_head(tip, direction, view)
+        cam = view.camera
+        cam_v = cam.perspective? ? tip - cam.eye : cam.direction
+        perp_v = direction.normalize * cam_v
+        flattened = cam_v * perp_v
+        offset = view.pixels_to_model(ARROW_HEAD_SIZE, tip)
+
+        view.draw(GL_LINE_STRIP, [
+          tip.offset(Geom.linear_combination(1, perp_v, -1, flattened), offset),
+          tip,
+          tip.offset(Geom.linear_combination(-1, perp_v, -1, flattened), offset)
+        ])
+      end
 
       # @return [Length]
       def length
         (measure_vector.length * @@scale.factor).to_l
       end
 
+      # @return [Geom::Vector3d]
       def measure_vector
         @end_ip.position - @start_ip.position
       end
